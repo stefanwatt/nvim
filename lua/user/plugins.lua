8a01151e774546551232d93cc97adafd2e9d6ca1
Stefan Watt <stefan.watt@dedalus.com>
2022-12-25 19:33:10 +0100

    before lazy.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -152,16 +152,17 @@ return packer.startup(function(use)
   use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" },
     commit = "c8ce83a66deb0ca6f5af5a9f9d5fcc05a6d0f66b" }
   use { "ravenxrz/DAPInstall.nvim", commit = "8798b4c36d33723e7bba6ed6e2c202f84bb300de" }
+  -- lua debug adapter
   use { "jbyuki/one-small-step-for-vimkind", commit = "ccd671fedaca36e474aadfdd70b9ca4189fcd86e" }
   use { "mxsdev/nvim-dap-vscode-js", commit = "079d0f3713c4649603290dc2ecc765e23e76a9fc",
     requires = { "mfussenegger/nvim-dap" } }
 
   -- Java
-  use { "mfussenegger/nvim-jdtls", commit = "75d27daa061458dd5735b5eb5bbc48d3baad1186" } -- END PLUGINS
+  use { "mfussenegger/nvim-jdtls", commit = "75d27daa061458dd5735b5eb5bbc48d3baad1186" } 
+  -- END PLUGINS
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
-  -- END PLUGINS
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
