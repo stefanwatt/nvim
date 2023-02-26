8e366f9ce5f9b249a5c6498204a67840204ee553
Stefan Watt <stefan.watt@gmail.com>
2023-02-26 20:23:45 +0100

    cleanup

--- lua/user/plugins/utility.lua
+++ lua/user/plugins/utility.lua
@@ -5,7 +5,6 @@ return {
   { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" },
   { "kylechui/nvim-surround", commit = "17191679202978b1de8c1bd5d975400897b1b92d" },
   { "GustavoKatel/tasks.nvim", commit = "1bb8b9725cc7bb58e646d9a8da48a57010cafcad" },
-  -- { "stefanwatt/auto-save.nvim" },
   {
     "https://git.sr.ht/~nedia/auto-save.nvim",
     event = "BufWinEnter",
@@ -13,7 +12,6 @@ return {
       require("auto-save").setup()
     end
   },
-  { "monaqa/dial.nvim", commit = "6bf54a83cd40448f1ba5171358f0d6f48bd970fd" },
   { "gbprod/yanky.nvim", commit = "39bef9fe84af59499cdb88d8e8fb69f3175e1265" },
   { "Susensio/magic-bang.nvim" },
   { "nvim-pack/nvim-spectre", dependencies={"nvim-lua/plenary.nvim"}}
