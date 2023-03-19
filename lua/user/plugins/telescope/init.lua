3c5dcafe3a825c53b3114851c7c86461224a14d3
Stefan Watt <stefan.watt@gmail.com>
2023-03-19 23:05:37 +0100

    deleted unnecessary files

--- lua/user/plugins/telescope/init.lua
+++ lua/user/plugins/telescope/init.lua
@@ -2,7 +2,49 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     config = function()
-      require("user.telescope")
+      local status_ok, telescope = pcall(require, "telescope")
+      if not status_ok then
+        return
+      end
+
+      local actions = require "telescope.actions"
+
+      telescope.setup {
+        defaults = {
+
+          prompt_prefix = " ",
+          selection_caret = " ",
+          path_display = { "smart" },
+          file_ignore_patterns = { ".git/", "node_modules" },
+
+          mappings = {
+            i = {
+              ["<Down>"] = actions.move_selection_next,
+              ["<Up>"] = actions.move_selection_previous,
+            },
+          },
+        },
+        extensions = {
+          menufacture = {
+            mappings = {
+              main_menu = { [{ 'n' }] = 'm' },
+            },
+          },
+          file_browser = {
+            theme = "dropdown",
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            mappings = {
+              ["i"] = {
+                -- your custom insert mode mappings
+              },
+              ["n"] = {
+                -- your custom normal mode mappings
+              },
+            },
+          },
+        },
+      }
     end
   },
   require("user.plugins.telescope.file-browser"),
