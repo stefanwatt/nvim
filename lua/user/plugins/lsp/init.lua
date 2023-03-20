8794ca0c34904b47492a59c032dc1972559076b4
Stefan Watt <stefan.watt@gmail.com>
2023-03-20 23:40:33 +0100

    removed some unneccessary files

--- lua/user/plugins/lsp/init.lua
+++ lua/user/plugins/lsp/init.lua
@@ -49,6 +49,30 @@ return {
       end
 
       lsp.setup_nvim_cmp({
+        mapping = cmp.mapping.preset.insert({
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+          ["<C-e>"] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+          }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, {
+            "i",
+            "s",
+          }),
+        }),
         formatting = {
           fields = { "kind", "abbr", "menu" },
           format = function(entry, vim_item)
