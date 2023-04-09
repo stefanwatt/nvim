c888549a6d58e5ce6ea68cee7d0a7e2c0b5bd4e5
Stefan Watt <stefan.watt@gmail.com>
2023-04-10 00:34:54 +0200

    - fixed broken noice config - adapted to new syntax

--- lua/user/plugins/ui/noice.lua
+++ lua/user/plugins/ui/noice.lua
@@ -2,7 +2,7 @@ return {
   {
     "stefanwatt/noice.nvim",
     event = "VeryLazy",
-
+    -- enabled = false,
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
@@ -19,60 +19,47 @@ return {
             enabled = false
           }
         },
-        cmdline = {
-          enabled = true, -- disable if you use native command line UI
-          view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
-          opts = { buf_options = { filetype = "vim" } }, -- enable syntax highlighting in the cmdline
-          icons = {
-            ["/"] = { icon = " ", hl_group = "DiagnosticWarn" },
-            ["?"] = { icon = " ", hl_group = "DiagnosticWarn" },
-            [":"] = { icon = " ", hl_group = "DiagnosticInfo", firstc = false },
-          },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = false,          -- use a classic bottom cmdline for search
         },
         messages = {
-          -- NOTE: If you enable noice messages UI, noice cmdline UI is enabled
-          -- automatically. You cannot enable noice messages UI only.
-          -- It is current neovim implementation limitation.  It may be fixed later.
-          enabled = false, -- disable if you use native messages UI
-        },
-        popupmenu = {
-          enabled = true, -- disable if you use something like cmp-cmdline
-          ---@type 'nui'|'cmp'
-          backend = "nui", -- backend to use to show regular cmdline completions
-          -- You can specify options for nui under `config.views.popupmenu`
-        },
-        history = {
-          -- options for the message history that you get with `:Noice`
-          view = "split",
-          opts = { enter = true },
-          filter = { event = "msg_show", ["not"] = { kind = { "search_count", "echo" } } },
+          enabled = false, -- enables the Noice messages UI
         },
         notify = {
-          -- Noice can be used as `vim.notify` so you can route any notification like other messages
-          -- Notification messages have their level and other properties set.
-          -- event is always "notify" and kind can be any log level as a string
-          -- The default routes will forward notifications to nvim-notify
-          -- Benefit of using Noice for this is the routing and consistent history view
           enabled = false,
         },
-        hacks = {
-          -- due to https://github.com/neovim/neovim/issues/20416
-          -- messages are resent during a redraw. Noice detects this in most cases, but
-          -- some plugins (mostly vim plugns), can still cause loops.
-          -- When a loop is detected, Noice exits.
-          -- Enable this option to simply skip duplicate messages instead.
-          skip_duplicate_messages = false,
+        views = {
+          cmdline_popup = {
+            position = {
+              row = "40%",
+              col = "50%",
+            },
+            size = {
+              width = 60,
+              height = "auto",
+            },
+          },
+          popupmenu = {
+            relative = "editor",
+            position = {
+              row = "55%",
+              col = "50%",
+            },
+            size = {
+              width = 60,
+              height = 10,
+            },
+            border = {
+              style = "rounded",
+              padding = { 0, 1 },
+            },
+            win_options = {
+              winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+            },
+          },
         },
-        throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
-        ---@type table<string, NoiceViewOptions>
-        views = {}, -- @see the section on views below
-        ---@type NoiceRouteConfig[]
-        routes = {}, -- @see the section on routes below
-        ---@type table<string, NoiceFilter>
-        status = {}, --@see the section on statusline components below
-        ---@type NoiceFormatOptions
-        format = {}, -- @see section on formatting
       })
-    end
-  }
+    end,
+  },
 }
