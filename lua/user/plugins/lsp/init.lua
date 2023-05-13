18ed368eeaddcba0bdc234d4ece9f92355367247
Stefan Watt <stefan.watt@gmail.com>
2023-05-13 14:57:42 +0200

    back to lspzero

--- lua/user/plugins/lsp/init.lua
+++ lua/user/plugins/lsp/init.lua
@@ -3,9 +3,22 @@ return {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
     lazy = false,
+    dependencies = {
+      { 'neovim/nvim-lspconfig' }, -- Required
+      {
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+      { 'hrsh7th/nvim-cmp' },     -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'L3MON4D3/LuaSnip' },     -- Required
+      { 'yioneko/nvim-vtsls'}
+    },
     config = function()
       require("neodev").setup({})
-      require('lsp-zero.settings').preset({})
       local lsp = require('lsp-zero').preset({})
       lsp.set_preferences({
         suggest_lsp_servers = true,
@@ -23,11 +36,19 @@ return {
         hint = '⚑',
         info = '»'
       })
+
+      lsp.on_attach(function(client, bufnr)
+        lsp.default_keymaps({ buffer = bufnr })
+      end)
+
+      local lspconfig = require('lspconfig')
+      lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+      -- require("lspconfig.configs").vtsls = require("vtsls").lspconfig -- set default server config
+      -- lspconfig.vtsls.setup({ --[[ your custom server config here ]] })
       lsp.setup()
     end
   },
   require("user.plugins.lsp.cmp"),
-  require("user.plugins.lsp.lspconfig"),
   require("user.plugins.lsp.null-ls"),
   require("user.plugins.lsp.lsp-saga"),
   require("user.plugins.lsp.lsp-lines"),
