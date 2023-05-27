483071a1d5a8242fa3c474027c442c572f3c16dd
Stefan Watt <stefan.watt@gmail.com>
2023-05-27 12:50:30 +0200

    - updated bufferline

--- lua/user/plugins/ui/bufferline.lua
+++ lua/user/plugins/ui/bufferline.lua
@@ -1,45 +1,43 @@
 return {
-  -- {
-  --   "akinsho/bufferline.nvim",
-  --   disabled = true,
-  --   event = "VeryLazy",
-  --   tag = "v3.0.0",
-  --   commit = "a40f058c6284855ad6a8b8137b3e312beca4d6aa",
-  --   dependencies = { "kyazdani42/nvim-web-devicons" },
-  --   config = function()
-  --     vim.opt.termguicolors = true
-  --
-  --     require('bufferline').setup {
-  --       options = {
-  --         mode = "buffers",                     -- set to "tabs" to only show tabpages instead
-  --         close_command = "bdelete! %d",        -- can be a string | function, see "Mouse actions"
-  --         right_mouse_command = "bdelete! %d",  -- can be a string | function, see "Mouse actions"
-  --         left_mouse_command = "buffer %d",     -- can be a string | function, see "Mouse actions"
-  --         middle_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-  --         diagnostics = "nvim_lsp",
-  --         diagnostics_update_in_insert = false,
-  --         custom_filter = function(buf_number, buf_numbers)
-  --           if vim.bo[buf_number].filetype ~= "alpha" then
-  --             return true
-  --           end
-  --           if vim.bo[buf_number].filetype ~= "NvimTree" then
-  --             return true
-  --           end
-  --         end,
-  --         offsets = {
-  --           {
-  --             filetype = "NvimTree",
-  --             text = "File Explorer",
-  --             text_align = "center",
-  --             separator = true
-  --           }
-  --         },
-  --         separator_style = "thick",
-  --         always_show_bufferline = true,
-  --       }
-  --     }
-  --
-  --     local toggle_bufferline = vim.api.nvim_create_augroup("ToggleBufferline", { clear = true })
-  --   end
-  -- }
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    version = "*",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+      vim.opt.termguicolors = true
+
+      require('bufferline').setup {
+        options = {
+          mode = "buffers",                     -- set to "tabs" to only show tabpages instead
+          close_command = "bdelete! %d",        -- can be a string | function, see "Mouse actions"
+          right_mouse_command = "bdelete! %d",  -- can be a string | function, see "Mouse actions"
+          left_mouse_command = "buffer %d",     -- can be a string | function, see "Mouse actions"
+          middle_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+          diagnostics = "nvim_lsp",
+          diagnostics_update_in_insert = false,
+          custom_filter = function(buf_number, buf_numbers)
+            if vim.bo[buf_number].filetype ~= "alpha" then
+              return true
+            end
+            if vim.bo[buf_number].filetype ~= "NvimTree" then
+              return true
+            end
+          end,
+          offsets = {
+            {
+              filetype = "NvimTree",
+              text = "File Explorer",
+              text_align = "center",
+              separator = true
+            }
+          },
+          separator_style = "thick",
+          always_show_bufferline = true,
+        }
+      }
+
+      local toggle_bufferline = vim.api.nvim_create_augroup("ToggleBufferline", { clear = true })
+    end
+  }
 }
