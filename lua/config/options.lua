5d1def6ad847c3f8892647090dd18b25922a5f6d
Stefan Watt <stefan.watt@gmail.com>
2023-10-14 15:50:27 +0200

    remove statusline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.o.timeoutlen = 500
+vim.o.cmdheight = 0
+vim.o.laststatus = 0
 vim.cmd([[
   if exists("g:neovide")
     set guifont=VictorMono\ Nerd\ Font:h12
@@ -19,11 +21,11 @@ vim.cmd([[
 ]])
 
 if vim.g.neovide then
-  vim.g.neovide_input_use_logo = 1            -- enable use of the logo (cmd) key
-  vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
-  vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
-  vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
-  vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
-  vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
-  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+	vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
+	vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 end
