c18c7c1c9081a735c08350b577f0ea9123a678cb
Stefan Watt <stefan.watt@gmail.com>
2024-01-12 15:25:41 +0100

    minipick,minifiles,keymaps,options

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -1,5 +1,40 @@
-return { 'echasnovski/mini.files', version = false, config=function()
-	require("mini.files").setup()
-end,
-}
+return {
+{ 
+	'echasnovski/mini.files',
+	version = false,
+	config=function()
+		require('mini.files').setup({
+				mappings = {
+					close = "q",
+					go_in_plus = "<Right>",
+					go_in = "L",
+					go_out_plus = "<Left>",
+					go_out = "H",
+					reset = "<BS>",
+					show_help = "?",
+					synchronize = "=",
+					trim_left = "<",
+					trim_right = ">",
+				},
+				options = {
+					-- Whether to use for editing directories
+					use_as_default_explorer = true,
+					fs_actions_confirm = false,
+					close_on_file_opened = true,
+					open_on_current_dir = true,
+				},
+    }) 
+	end,
+	keys = {
+		{
+			"<leader>e",
+			mode={"n"},
+			function()
+				MiniFiles.open() 
+			end,
+			desc="File Explorer"
+		}
 
+		}
+	},
+}
