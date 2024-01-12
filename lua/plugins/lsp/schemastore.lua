39562e255948b287e7a68453c4426f8396f3929b
Stefan Watt <stefan.watt@gmail.com>
2024-01-12 14:59:15 +0100

    -foo

--- lua/plugins/lsp/schemastore.lua
+++ lua/plugins/lsp/schemastore.lua
@@ -2,12 +2,12 @@ return {
 	"b0o/schemastore.nvim",
 	dependencies = { "neovim/nvim-lspconfig" },
 	event = "VeryLazy",
+	version = false,
 	config = function()
 		local lspconfig = require("lspconfig")
 		local schemastore = require("schemastore")
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities.textDocument.completion.completionItem.snippetSupport = true
-
 		lspconfig.jsonls.setup({
 			capabilities = capabilities,
 			settings = {
