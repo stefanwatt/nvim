aed8c197b8f3c90ad2dbc0da0aebac95f64552c7
Stefan Watt <stefan.watt@gmail.com>
2024-01-12 16:35:18 +0100

    cmp is now working better

--- lua/plugins/lsp/lsp-zero.lua
+++ lua/plugins/lsp/lsp-zero.lua
@@ -25,6 +25,18 @@ return {
         lsp_zero.default_setup,
       },
     })
+    local cmp = require('cmp')
+    local cmp_action = require('lsp-zero').cmp_action()
+
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<Tab>'] = cmp_action.luasnip_jump_forward(),
+        ['<S-Tab>'] = cmp_action.luasnip_jump_backward(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      })
+    })
   end,
   keys = {
 			{
