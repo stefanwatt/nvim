e342931f36a30364d48e30df05a708fc83568a83
Stefan Watt <stefan.watt@gmail.com>
2024-06-09 00:17:33 +0200

    - removed copilot from cmp sources - now instead i use virtual text and only manual trigger

--- lua/config/nvim-gui.lua
+++ lua/config/nvim-gui.lua
@@ -154,8 +154,6 @@ local TSCallbackNames = {
 ---@type number
 local counter = 0
 
-local M = {}
-
 local function escape_special_characters(str)
     local replacements = {
         ["="] = "eq",
@@ -196,6 +194,7 @@ local function escape_special_characters(str)
 		["="] = "eq",
 		["."] = "dot",
 		[","] = "comma",
+
 		[" "] = "space",
 		["/"] = "slash",
 		["\\"] = "backslash",
@@ -330,9 +329,9 @@ M.attach_buffer = function(channel)
 	end
 	language_tree:register_cbs({
 		on_changedtree = function(ranges, ts_tree)
-			local tree = M.get_tree_as_table(ts_tree:root(), language_tree:lang(), bufnr)
-			-- vim.print(tree)
-			vim.fn.rpcrequest(channel, "nvim-gui-buf-changed", tree)
+			local tokens = M.get_tokens(ts_tree:root(), language_tree:lang())
+			print("got tokens " .. tostring(#tokens))
+			vim.fn.rpcrequest(channel, "nvim-gui-buf-changed", tokens)
 		end,
 	}, true)
 	return "success"
@@ -358,6 +357,27 @@ local function get_hl_captures(bufnr, node, lang)
     return query:iter_captures(node, bufnr, 0, -1), query.captures
 end
 
+---@param base_id string
+---@param seen_token_ids string[]
+local function generate_unique_id(base_id, seen_token_ids)
+	local token_id = base_id
+	local suffix = 0
+	while seen_token_ids[token_id] do
+		suffix = suffix + 1
+		token_id = base_id .. tostring(suffix)
+	end
+	return token_id
+end
+
+local function hash(str)
+	local hash = 5381
+	for i = 1, #str do
+		local char = str:byte(i)
+		hash = ((hash * 33) + char) % 2 ^ 32 -- Ensuring it stays within 32-bit integer range
+	end
+	return hash
+end
+
 ---@param root TSNode
 ---@param lang vim.treesitter.Language
 M.get_tokens = function(root, lang)
@@ -366,6 +386,53 @@ M.get_tokens = function(root, lang)
 	if not iterator or captures == {} then
 		return nil
 	end
+
+	local tokens = {}
+	local seen_token_ids = {}
+	for id, node, _ in iterator do
+		local node_start_row, node_start_col, node_end_row, node_end_col = node:range()
+		local token = {
+			start_row = node_start_row,
+			start_col = node_start_col,
+			end_row = node_end_row,
+			end_col = node_end_col,
+		}
+		local capture_name = captures[id]
+		local ok, text =
+			pcall(vim.api.nvim_buf_get_text, bufnr, node_start_row, node_start_col, node_end_row, node_end_col, {})
+		token.text = ok and text[1] or ""
+
+		-- local base_id = node:id() .. escape_special_characters(token.text)
+		-- base_id = hash(base_id)
+		-- local token_id = generate_unique_id(base_id, seen_token_ids)
+
+		token.id = node:id() .. token.text
+		-- seen_token_ids[token_id] = true
+
+		token.hl_group = capture_name
+
+		local hl_id = vim.api.nvim_get_hl_id_by_name(capture_name)
+		local highlights = vim.api.nvim_get_hl(0, { name = "@" .. capture_name, link = false })
+			or vim.api.nvim_get_hl(0, { name = capture_name, link = false })
+
+		for k, v in pairs(highlights) do
+			if k == "fg" or k == "bg" then
+				token[k] = tostring(M.decimal_to_hex_color(v))
+			else
+				token[k] = v
+			end
+		end
+		table.insert(tokens, token)
+	end
+
+---@param root TSNode
+---@param lang vim.treesitter.Language
+M.get_tokens = function(root, lang)
+    local bufnr = vim.api.nvim_get_current_buf()
+    local iterator, captures = get_hl_captures(bufnr, root, lang)
+    if not iterator or captures == {} then
+        return nil
+    end
 
 	local tokens = {}
 	for id, node, _ in iterator do
@@ -642,4 +709,72 @@ M.print_tree_as_table = function()
 	write_to_buffer(tree_table)
 end
 
+local function get_hl_of_node(node, bufnr, query)
+	local start_row, start_col, end_row, end_col = node:range()
+	local iterator = query:iter_captures(node, bufnr, start_row, end_row)
+	local tokens = {}
+	for id, child, _ in iterator do
+		local child_start_row, child_start_col, child_end_row, child_end_col = child:range()
+		local token = {
+			start_row = child_start_row,
+			start_col = child_start_col,
+			end_row = child_end_row,
+			end_col = child_end_col,
+		}
+		local capture_name = query.captures[id]
+		local ok, text =
+			pcall(vim.api.nvim_buf_get_text, bufnr, child_start_row, child_start_col, child_end_row, child_end_col, {})
+		token.text = ok and text[1] or ""
+		token.hl_group = capture_name
+		local highlights = vim.api.nvim_get_hl(0, { id = id })
+		for k, v in pairs(highlights) do
+			if k == "foreground" or k == "background" then
+				token[k] = tostring(M.decimal_to_hex_color(v))
+			else
+				token[k] = v
+			end
+		end
+		table.insert(tokens, token)
+	end
+	local ok, text = pcall(vim.api.nvim_buf_get_text, bufnr, start_row, start_col, end_row, end_col, {})
+	return tokens, ok and text[1] or ""
+end
+
+local function walk_node(node, depth, query)
+	for child, name in node:iter_children() do
+		local hl, text = get_hl_of_node(child, 0, query)
+		local indentation = string.rep("  ", depth)
+		print(
+			indentation .. " text: '" .. text .. "' type:",
+			child:type(),
+			" name: ",
+			name,
+			" highlights: ",
+			vim.inspect(hl)
+		)
+		if child:child_count() > 0 then
+			walk_node(child, depth + 1, query)
+		end
+	end
+end
+
+M.get_tree_as_table = function()
+	local bufnr = vim.api.nvim_get_current_buf()
+	local language_tree = parsers.get_parser(bufnr)
+	local tree = language_tree:parse()[1]
+	local lang = language_tree:lang()
+	local root = tree:root()
+	local query = vim.treesitter.query.get(lang, "highlights")
+	walk_node(root, 0, query)
+end
+
+M.print_tokens = function()
+	local bufnr = vim.api.nvim_get_current_buf()
+	local language_tree = parsers.get_parser(bufnr)
+	local tree = language_tree:parse()[1]
+	local lang = language_tree:lang()
+	local root = tree:root()
+	local tokens = M.get_tokens(root, lang)
+	vim.print(tokens)
+end
 return M
