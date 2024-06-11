29aaeed3df4736d3dd12e7db06497acb107b963c
Stefan Watt <stefan.watt@gmail.com>
2024-06-11 21:07:04 +0200

    added conform toggle with notifictation

---
+++ lua/plugins/utility/init.lua
@@ -0,0 +1,12 @@
+return {
+	require("plugins.utility.tasks"),
+	require("plugins.utility.notify"),
+	require("plugins.utility.auto-session"),
+	require("plugins.utility.persistence"),
+	require("plugins.utility.copilot-chat"),
+	require("plugins.utility.comment"),
+	require("plugins.utility.surround"),
+	require("plugins.utility.harpoon"),
+	require("plugins.utility.mini-files"),
+	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+}
