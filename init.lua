27b1c577a09dbd511bcfd0c8192156b810b9c127
Stefan Watt <stefan.watt@gmail.com>
2024-07-19 00:09:24 +0200

    fugitive+telescope = diff

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 require("config.options")
 require("config.usercmds")
-require("config.autocmds") --require
+require("config.autocmds")
 require("config.keymaps")
 require("config.lazy")
 -- require("config.cmdline")
