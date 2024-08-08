7928a64eb0d8d2991a2a547edd45ad75a120b788
Stefan Watt <stefan.watt@gmail.com>
2024-08-08 15:10:59 +0200

    - no luasnip cmp in markdown - no copilot anymore

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -11,13 +11,12 @@ return { -- Autocompletion
 		local luasnip = require("luasnip")
 		luasnip.config.setup({})
 
-		cmp.event:on("menu_opened", function()
-			vim.b.copilot_suggestion_hidden = true
-		end)
-
-		cmp.event:on("menu_closed", function()
-			vim.b.copilot_suggestion_hidden = false
-		end)
+		cmp.setup.filetype('markdown', {
+			sources = {
+				{ name = 'buffer' },
+				{ name = 'path' },
+			},
+		})
 
 		cmp.setup({
 			snippet = {
@@ -31,7 +30,7 @@ return { -- Autocompletion
 				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				-- ["<C-Space>"] = cmp.mapping.complete(),
+				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<Tab>"] = cmp.mapping.confirm({ select = true }),
 				["<S-CR>"] = cmp.mapping.confirm({
