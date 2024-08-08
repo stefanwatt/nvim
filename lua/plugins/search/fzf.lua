623951c2376a265cdb6f1b2bbf2d32003bb205e9
Stefan Watt <stefan.watt@gmail.com>
2024-08-08 02:11:58 +0200

    fzf-lua + todo-comments

--- lua/plugins/search/fzf.lua
+++ lua/plugins/search/fzf.lua
@@ -5,7 +5,7 @@ return {
 		"ahmedkhalf/project.nvim",
 	},
 	keys = {
-		{ "<leader>f", name = "Find" },
+		{ "<leader>f",  name = "Find" },
 		{
 			"gd",
 			"<cmd>lua require('fzf-lua').lsp_definitions({ jump_to_single_result = true })<CR>",
@@ -22,10 +22,15 @@ return {
 			desc = "[f]ind [r]eferences",
 		},
 		{ "<leader>fs", "<cmd>FzfLua lsp_document_symbols<cr>", desc = "[f]ind [s]ymbols" },
-		{ "<leader>fh", "<cmd>FzfLua helptags<cr>", desc = "[f]ind [h]elp" },
-		{ "<leader>fp", "<cmd>FzfProject<cr>", desc = "[f]ind [p]roject" },
-		{ "<leader>ff", "<cmd>FzfLua git_files<cr>", desc = "[f]ind [f]iles" },
-		{ "<leader>fw", "<cmd>FzfLua live_grep<cr>", desc = "[f]ind [w]ord" },
+		{ "<leader>fh", "<cmd>FzfLua helptags<cr>",             desc = "[f]ind [h]elp" },
+		{ "<leader>fp", "<cmd>FzfProject<cr>",                  desc = "[f]ind [p]roject" },
+		{ "<leader>ff", "<cmd>FzfLua git_files<cr>",            desc = "[f]ind [f]iles" },
+		{ "<leader>fw", "<cmd>FzfLua live_grep<cr>",            desc = "[f]ind [w]ord" },
+		{
+			"<leader>ft",
+			"<cmd>lua require('fzf-lua').grep({search='TODO|HACK|PERF|NOTE|FIXME|FIX', no_esc=true})<CR>",
+			desc = "[f]ind [t]odo",
+		},
 	},
 	config = function()
 		local fzflua = require("fzf-lua")
--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -6,6 +6,6 @@ return {
   keys = {
     { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
     { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
-    { "<leader>tq", "<cmd>TodoQuickfix<CR>", desc="[t]odo [q]uickfix"}
+    { "<leader>tq", "<cmd>TodoQuickFix<CR>", desc="[t]odo [q]uickfix"}
   },
 }
